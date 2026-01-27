270927b928cfa03dbf5db6c700d5f75b36021914
Debola <adebolabalogun5@gmail.com>
2026-01-27 14:42:19 +0100

    created the abis template for xreserve

---
+++ frontend/src/lib/contracts/abis/xReserve.ts
@@ -0,0 +1,25 @@
+export const xReserveAbi = [
+  {
+    name: "deposit",
+    type: "function",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "amount", type: "uint256" },
+      { name: "destinationChainId", type: "uint32" },
+      { name: "mintRecipient", type: "bytes32" },
+    ],
+    outputs: [{ name: "nonce", type: "uint64" }],
+  },
+  {
+    name: "depositForBurn",
+    type: "function",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "amount", type: "uint256" },
+      { name: "destinationDomain", type: "uint32" },
+      { name: "mintRecipient", type: "bytes32" },
+      { name: "burnToken", type: "address" },
+    ],
+    outputs: [{ name: "nonce", type: "uint64" }],
+  },
+] as const;
