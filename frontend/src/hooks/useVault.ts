b440b324e7dc9c87cd54a8338b6e40369ff21651
Debola <adebolabalogun5@gmail.com>
2026-01-30 13:50:00 +0100

    added algorithm for effect

--- frontend/src/hooks/useVault.ts
+++ frontend/src/hooks/useVault.ts
@@ -5,4 +5,13 @@ import { useVaultStore } from "@/stores/useVaultStore";
 import { useWalletStore } from "@/stores/useWalletStore";
 import { useUIStore } from "@/stores/useUIStore";
 
+export function useVault() {
+  const store = useVaultStore();
+  const stxAddress = useWalletStore((s) => s.stxAddress);
+  const addToast = useUIStore((s) => s.addToast);
 
+  useEffect(() => {
+    store.fetchVaultStats();
+  }, []);
+
+}
