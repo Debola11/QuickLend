e1ee1dd99e0e817b8210d96ce062e1dd1ede16f2
Debola <adebolabalogun5@gmail.com>
2026-01-31 02:17:26 +0100

    added interface

--- frontend/src/components/bridge/AmountInput.tsx
+++ frontend/src/components/bridge/AmountInput.tsx
@@ -1,3 +1,12 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+
+interface AmountInputProps {
+  value: string;
+  onChange: (value: string) => void;
+  maxAmount?: string;
+  tokenSymbol: string;
+  error?: string;
+  disabled?: boolean;
+}
